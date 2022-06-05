cbad43ab1d6e7dd0417fac9e2d4a3b34d9103df3
tizarus <50199109+tizarus@users.noreply.github.com>
2022-06-04 22:14:29 -0500

    added code to rename index column

--- csvtodb.py
+++ csvtodb.py
@@ -4,10 +4,15 @@ from sqlalchemy.types import Integer, Text, String, DateTime
 import pymysql
 
 # database engine
-engine = create_engine("DATABASE_URL")
+engine = create_engine("mysql+pymysql://root:admin@localhost:3306/exercise_db")
 
 # import csv file and convert to dataframe
 df = pd.read_csv("exercise_database.csv")
 
+# rename index column to id
+df.index.name = 'id'
+
+print(df)
+
 # import dataframe into database
-df.to_sql(con=engine, name="exercises", if_exists="replace")
+df.to_sql(con=engine, name="exercise", if_exists="replace")
