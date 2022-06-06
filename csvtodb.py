7f1dec744b2fbb276c2c2fdee4051f3dd7ba9dda
tizarus <50199109+tizarus@users.noreply.github.com>
2022-06-06 10:57:54 -0500

    added code to change index to 1 from 0

--- csvtodb.py
+++ csvtodb.py
@@ -9,6 +9,9 @@ engine = create_engine("mysql+pymysql://root:admin@localhost:3306/exercise_db")
 # import csv file and convert to dataframe
 df = pd.read_csv("exercise_database.csv")
 
+#set index to 1 from 0
+df.index = df.index + 1
+
 # rename index column to id
 df.index.name = 'id'
 
