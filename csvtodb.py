3df416aa21fe60d7b58c0dbcb3784311b8678dcc
tizarus <50199109+tizarus@users.noreply.github.com>
2022-06-08 01:05:58 -0500

    script to convert .csv file to mysql database

--- csvtodb.py
+++ csvtodb.py
@@ -4,7 +4,7 @@ from sqlalchemy.types import Integer, Text, String, DateTime
 import pymysql
 
 # database engine
-engine = create_engine("mysql+pymysql://root:admin@localhost:3306/exercise_db")
+engine = create_engine("mysql+pymysql://username:password@localhost:3306/exercise_db")
 
 # import csv file and convert to dataframe
 df = pd.read_csv("exercise_database.csv")
